367c22eacdb16c1d5fc50e76411b00a88e785c65
Tino Hager <tino.hager@nager.at>
2024-07-16 22:55:56 +0200

    initial commit

---
+++ src/Frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Mailtower E-Mail Header Analyzer
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ src/Frontend/src/pages/IndexPage.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+interface HeaderDetails {
+  headerName: string
+  headerData: string
+}
+
+const mailHeader = ref<string>()
+
+const mailHeaderParts = computed(() => {
+  const lines = mailHeader.value?.split(/\r?\n/)
+  if (!lines) {
+    return undefined
+  }
+
+  const formattedHeader = []
+  let currentLine = ''
+
+  for (const line of lines) {
+    if (!line) {
+      break
+    }
+
+    if (/^\s/.test(line)) {
+      // Line is a continuation of the previous header field
+      currentLine += line.trim()
+    } else {
+      // Line is a new header field
+      if (currentLine) {
+        formattedHeader.push(decodeHeaderField(currentLine))
+      }
+      currentLine = line
+    }
+  }
+
+  // Push the last accumulated line
+  if (currentLine) {
+    formattedHeader.push(decodeHeaderField(currentLine))
+  }
+
+  // Join the formatted header lines with new lines
+  return formattedHeader
+})
+
+function decodeHeaderField (field: string): HeaderDetails {
+  const rawHeaderField = field.replace(/=\?([^?]+)\?([BQ])\?([^?]+)\?=/gi, (_, charset, encoding, encodedText) => {
+    if (encoding.toUpperCase() === 'B') {
+      return decodeBase64(encodedText, charset)
+    } else if (encoding.toUpperCase() === 'Q') {
+      return decodeQuotedPrintable(encodedText, charset)
+    }
+    return field
+  })
+
+  const indexOfFirstColon = rawHeaderField.indexOf(':')
+
+  return {
+    headerName: rawHeaderField.slice(0, indexOfFirstColon),
+    headerData: rawHeaderField.slice(indexOfFirstColon + 1)
+  }
+}
+
+function decodeBase64 (encodedText: string, charset: string): string {
+  const decodedText = atob(encodedText)
+  return new TextDecoder(charset).decode(new Uint8Array([...decodedText].map(char => char.charCodeAt(0))))
+}
+
+function decodeQuotedPrintable (encodedText: string, charset: string): string {
+  const decodedText = encodedText.replace(/_/g, ' ').replace(/=([A-Fa-f0-9]{2})/g, (_, hex) => {
+    return String.fromCharCode(parseInt(hex, 16))
+  })
+  return new TextDecoder(charset).decode(new Uint8Array([...decodedText].map(char => char.charCodeAt(0))))
+}
+
+</script>
+
+<template>
+  <q-page padding>
+    <q-input
+      v-model="mailHeader"
+      outlined
+      type="textarea"
+    />
+
+    <q-list separator>
+      <q-item
+        v-for="(header, index) in mailHeaderParts"
+        :key="index"
+        dense
+      >
+        <q-item-section
+          no-wrap
+          style="word-break: break-all; white-space: normal;"
+        >
+          <q-badge>{{ header.headerName }}</q-badge>
+          {{ header.headerData }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    {{ mailHeaderParts?.length }}
+  </q-page>
+</template>
